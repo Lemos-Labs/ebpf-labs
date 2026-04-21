7d8d9479295f63bd8c4ba110dd6ce023c16a8f23
Eduardo Lemos <eduardolemosep@gmail.com>
2026-04-21 04:29:21 -0300

    fix: return bpf hook

--- hello-ebpf/src/bpf/hello.bpf.c
+++ hello-ebpf/src/bpf/hello.bpf.c
@@ -1,12 +1,13 @@
+
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-SEC("ksyscall/execve");
-int BPF_KSYSCALL(hello, const char, *pathname) 
+SEC("ksyscall/execve")
+int BPF_KSYSCALL(hello, const char *pathname)
 {
-    bpf_printk("Hello, world!");
-    exit 0;
+    bpf_printk("Hello World!");
+    return 0;
 }
